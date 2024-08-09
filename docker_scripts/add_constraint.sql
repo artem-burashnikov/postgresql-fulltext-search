68817cc75e3fc72e840841378ccad9bc3dc5ab5d
artem-burashnikov <artem.burashnikov@gmail.com>
2024-08-10 01:24:24 +0300

    feat: add docker sql alter script

---
+++ docker_scripts/add_constraint.sql
@@ -0,0 +1,1 @@
+ALTER TABLE text_2024.classical_literature ADD PRIMARY KEY (author, literature);
---
+++ docker_scripts/init-database.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS text_2024;
+
+ALTER DATABASE postgres SET search_path TO text_2024, public;
+
+CREATE TABLE IF NOT EXISTS text_2024.classical_literature(
+    author character varying(32) NOT NULL,
+    literature TEXT NOT NULL
+);
