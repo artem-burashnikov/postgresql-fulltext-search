b9becbf9f439c0c0860b236ac670c1b380d9bc0b
artem-burashnikov <artem.burashnikov@gmail.com>
2024-09-17 00:51:04 +0300

    fix:  initialization scripts

--- scripts/add-tsvector.sql
+++ scripts/add-tsvector.sql
@@ -1,3 +1,3 @@
-ALTER TABLE text_2024.classical_literature ADD column ts_text tsvector;
-
-UPDATE text_2024.classical_literature SET ts_text = to_tsvector('russian', text_2024.classical_literature.text);
+ALTER TABLE text_2024.classical_literature
+    ADD COLUMN ts_text tsvector
+               GENERATED ALWAYS AS (to_tsvector('russian', text_2024.classical_literature.text)) STORED;
--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS text_2024.classical_literature;
+CREATE SCHEMA IF NOT EXISTS text_2024;
 
 CREATE TABLE IF NOT EXISTS text_2024.classical_literature(
     id SERIAL,
@@ -9,10 +9,3 @@ CREATE TABLE IF NOT EXISTS text_2024.classical_literature(
 
 ALTER TABLE text_2024.classical_literature
     ADD CONSTRAINT pk_classical_literature PRIMARY KEY (id);
-
-ALTER TABLE IF EXISTS text_2024.classical_literature
-    OWNER to text_2024;
-
-GRANT ALL PRIVILEGES ON TABLE text_2024.classical_literature TO text_2024;
-
-GRANT USAGE, SELECT ON SEQUENCE text_2024.classical_literature_id_seq TO text_2024;
